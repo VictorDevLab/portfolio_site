4a35960cbb4266b5e0d3ba63901d9e0756748372
Victor Koome <koomev786@gmail.com>
2024-03-18 13:22:53 +0300

    add intro section

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function PortfolioItem({ title, imgUrl, stack, link }) {
    return (
-      <a 
+      <a
          href={link}
          target="_blank"
          rel="noopener noreferrer"
@@ -10,8 +10,8 @@ function PortfolioItem({ title, imgUrl, stack, link }) {
       >
          <img
             src={imgUrl}
-            alt="portfolio" 
-            className="w-full h-36 md:h-48 object-cover cursor-pointer"
+            alt="portfolio"
+            className="w-full h-44 md:h-48 object-cover cursor-pointer"
          />
          <div className="w-full p-4">
             <h3 className="text-lg md:text-xl dark:text-white mb-2 md:mb-3 font-semibold ">{title}</h3>

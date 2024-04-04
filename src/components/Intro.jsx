fe573408049361f8c0773a1bd1c6b3827f063c93
Victor Koome <koomev786@gmail.com>
2024-04-04 23:32:44 +0300

    change head description

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,9 +5,9 @@ function Intro() {
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
 
          <h1 className="text-4xl md:text-7xl dark:text-white mb-1 md:mb-3 font-bold">Victor</h1>
-         <p className="text-base md:text-xl mb-3 font-medium">Front-end Developer</p>
+         <p className="text-base md:text-xl mb-3 font-medium">Software Engineer & Web Developer</p>
          <p className="text-sm max-w-xl mb-6 font-bold">
-            I'm a front-end developer who thrives on turning design challenges into beautiful, user-friendly
+            I'm a web developer who thrives on turning design challenges into beautiful, user-friendly
             web experiences.
             I bring strong skills in HTML, CSS (including Tailwind), JavaScript, TypeScript,and  fameworks like React. Additionally I use Docker (for streamlined deployments and containerization).
             <br />
